556c5baab0a57ab2886485a8b25583033286cdb6
ADoseOfCode <140978248+ADoseOfCodeYT@users.noreply.github.com>
2025-05-06 18:14:37 +0100

    updated console, readme and cmakelists

--- DoctrinaEngine/Source/Tools/Private/deConsole.cpp
+++ DoctrinaEngine/Source/Tools/Private/deConsole.cpp
@@ -8,57 +8,11 @@ namespace de
 
 	void Console::Post(const char* message, LogLevel level)
 	{
-
-#if DEBUG
-		std::string levelText;
-
-		switch (level)
-		{
-		case Console::LogLevel::Default:
-			levelText = "";
-			break;
-		case Console::LogLevel::Warning:
-			levelText = "[WARNING] ";
-			break;
-		case Console::LogLevel::Error:
-			levelText = "[ERROR] ";
-			break;
-		case Console::LogLevel::ExtremeError:
-			levelText = "[EXTREME ERROR] ";
-			break;
-		default:
-			break;
-		}
-
-		std::cout << "" + levelText + message << std::endl;
-#endif // DEBUG
+		// TODO: CONSOLE UI TO PRINT THE MESSAGE TO 
 	}
 
 	void Console::Post(const std::string& message, LogLevel level)
 	{
-
-#if DEBUG
-		std::string levelText;
-
-		switch (level)
-		{
-		case Console::LogLevel::Default:
-			levelText = "";
-			break;
-		case Console::LogLevel::Warning:
-			levelText = "[WARNING] ";
-			break;
-		case Console::LogLevel::Error:
-			levelText = "[ERROR] ";
-			break;
-		case Console::LogLevel::ExtremeError:
-			levelText = "[EXTREME ERROR] ";
-			break;
-		default:
-			break;
-		}
-
-		std::cout << "" + levelText + message << std::endl;
-#endif // DEBUG
+		// TODO: CONSOLE UI TO PRINT THE MESSAGE TO 
 	}
 }
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Doctrina SDK
  
 Doctrina SDK is a small hobby SDK with a 3D C++ game engine and an editor that uses the game engine.
-There are no plans to make fully-fledged games with this sdk
+There are no plans to make fully-fledged games with this sdk. 
+<br>
+How did I come up with the name Doctrina? 
+Well, Doctrina is Latin for learning and the point of this SDK is to learn how to make game engine.
 
 - [Trello Board](https://trello.com/b/0X4gj5H7/doctrina-sdk)
 
