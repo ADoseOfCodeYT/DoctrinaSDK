9a82583275f4fa049b420135dfedc4c9722e5e88
ADoseOfCode <140978248+ADoseOfCodeYT@users.noreply.github.com>
2025-05-11 15:34:06 +0100

    Update deVersion.h

--- DoctrinaEngine/Source/Core/Public/deVersion.h
+++ DoctrinaEngine/Source/Core/Public/deVersion.h
@@ -6,8 +6,8 @@
 namespace de::Version
 {
 	const int ENGINE_MAJOR = 0;
-	const int ENGINE_MINOR = 2;
-	const int ENGINE_REVISION = 4;
+	const int ENGINE_MINOR = 3;
+	const int ENGINE_REVISION = 5;
 
 	const std::string ENGINE_VERSION = std::to_string(ENGINE_MAJOR) + "." + std::to_string(ENGINE_MINOR) + "." + std::to_string(ENGINE_REVISION);
 }
